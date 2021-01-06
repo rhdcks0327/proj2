c9b1b0fc0f8bac68eb21f3518e36b8d3edfe139e
rhdcks0327 <76501258+rhdcks0327@users.noreply.github.com>
2021-01-06 16:25:58 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,5 +15,47 @@ $(document).ready(function(){//실행틀 시작
    $('#m_nav').hover(function(){$(this).stop().css({height:'15em','background-color':'rgba(255,255,255,1)'});
                        },function(){$(this).stop().css({height:'3.2em','background-color':'rgba(255,255,255,0)'})
                        })
-                       
+ $(document).ready(function(){
+
+$('.center').slick({
+  centerMode: true,
+  slidesToShow: 3,
+ /*responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]*/
+});
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+		
+});                      
                     })
