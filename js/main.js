79d4d4b27952c023a0d4bced6ce94fb20933111c
rhdcks0327 <76501258+rhdcks0327@users.noreply.github.com>
2021-01-08 10:30:18 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,45 +1,22 @@
 $(document).ready(function(){//실행틀 시작
-    $('.accordion').each(function(){
-       var allDt = $(this).find('dt');
-       var allDd = $(this).find('dd');
-            $('.on').next().slideDown();
-       $(allDt).click(function(){
-          allDt.removeClass('on')
-          allDd.slideUp();
-          $(this).addClass('on');
-          $(this).next().slideDown();
-            //prev()
-        })
-     })
-
    $('#m_nav').hover(function(){$(this).stop().css({height:'15em','background-color':'rgba(255,255,255,1)'});
                        },function(){$(this).stop().css({height:'3.2em','background-color':'rgba(255,255,255,0)'})
-                       })
+                       });
  $(document).ready(function(){
 
-$('.center').slick({
-  centerMode: true,
-  slidesToShow: 3,
- /*responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]*/
+
+$(".lazy").slick({
+  lazyLoad: 'ondemand', // ondemand progressive anticipated
+  infinite: true,
+  arrows : false,
+  speed : 1400,
+  dots : true 
+});
+$(".variable").slick({
+  dots: false,
+  infinite: true,
+  variableWidth: true,
+  arrows : true
 });
  $('.slider-for').slick({
   slidesToShow: 1,
